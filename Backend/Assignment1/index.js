1235f31e19146d5292f6390a903e1a2e99fe198f
3003abhishek <3003abhishek.jha@gmail.com>
2023-07-04 10:54:32 +0530

    practice reading and writing file both synchronously and asynchronously

--- Backend/Assignment1/index.js
+++ Backend/Assignment1/index.js
@@ -1,14 +1,43 @@
 const fs=require("fs");
 
 
-let inputText=fs.readFileSync("./folder/file1.txt","utf-8");
-let output=`The output data is :${inputText} and is printed on ${Date.now()}`;
-fs.writeFileSync("./folder/file2.txt",output);
+// let inputText=fs.readFileSync("./folder/file1.txt","utf-8");
+// let output=`The output data is :${inputText} and is printed on ${Date.now()}`;
+// fs.writeFileSync("./folder/file2.txt",output);
 
 
 
 
-console.log(inputText);
+
+// fs.readFile("./folder/file3.txt","utf-8",(err,data1)=>{
+//     fs.readFile(`./folder/${data1}.txt`,"utf-8",(err,data2)=>{
+//         console.log(data2);
+//     })
+// })
+
+
+
+const http=require("http");
+
+let server=http.createServer((req,res)=>{
+    res.end("Welcome to your nodes server");
+
+});
+
+
+
+
+
+server.listen(8080,()=>{
+    console.log("Your server is running on port 8080");
+})
+
+
+
+
+
+
+// console.log(inputText);
 
 
 
