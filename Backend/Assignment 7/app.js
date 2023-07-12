0d43c4cb200c3334c00be04046a1701b242bb502
3003abhishek <3003abhishek.jha@gmail.com>
2023-07-12 12:58:46 +0530

    leart how to respond to url parameters

--- Backend/Assignment 7/app.js
+++ Backend/Assignment 7/app.js
@@ -4,14 +4,47 @@ const fs=require("fs");
 
 const app=express();
 
-let tours=JSON.parse(fs.readFileSync("./dev_data/tours.json","utf-8"));
-console.log(tours);
+app.use(express.json());//This is used to post the json data in the file
 
-app.get("/",(req,res)=>{
+let tours=JSON.parse(fs.readFileSync("./dev_data/tours.json","utf-8"));
+// console.log(tours);
+
+app.get("/tours",(req,res)=>{
    res.status(200).json({
     "status":"Success",
     tours
    });
+});
+
+
+app.get("/tours/:id",(req,res)=>{
+   let id=req.params.id;
+   // if(id>23||!id){
+   //    res.json({"status":"error","message":"Please enter a valid id "});
+   // }
+   // else{
+       let tour=tours.filter((el)=>{
+         return el._id===id;
+      });
+
+      res.json({"status":"Success","data":tour});
+   // }
+
+
+})
+
+
+app.post("/tours",(req,res)=>{
+   let data=req.body;
+
+   tours.push(data);
+
+   fs.writeFile("./dev_data/toursWrite.json",JSON.stringify(tours),err=>{
+      res.status(201).json({"status":"created",tours});
+   }
+   
+   )
+   
 });
 
 
